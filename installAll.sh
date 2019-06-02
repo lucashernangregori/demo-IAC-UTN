3b1dbf7dd9ede1bfdd3030f904f25966682aa15b
root <root@LAPTOP-3ENO4CGG.localdomain>
2019-06-02 15:32:43 -0300

    metricbeat

--- installAll.sh
+++ installAll.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./ansibleBootstraper.sh
 ansible-playbook dockerInstall.yml
+ansible-playbook metricbeat_linux.yml
 ansible-playbook teamcityInstall.yml -l servicios
 ansible-playbook elasticSearchKibanaInstall.yml -l servicios
 ansible-playbook dotnetcoreInstall.yml -l webserver
