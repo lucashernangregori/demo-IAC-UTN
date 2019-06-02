ad9aa79be58f72fd56596e9395ba066c3eeac5fa
root <root@LAPTOP-3ENO4CGG.localdomain>
2019-06-02 15:44:53 -0300

    agrego filebeat

--- installAll.sh
+++ installAll.sh
@@ -5,3 +5,4 @@ ansible-playbook metricbeat_linux.yml
 ansible-playbook teamcityInstall.yml -l servicios
 ansible-playbook elasticSearchKibanaInstall.yml -l servicios
 ansible-playbook dotnetcoreInstall.yml -l webserver
+ansible-playbook filebeat_linux.yml -l webserver
