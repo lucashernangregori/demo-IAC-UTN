58b85bdcc8c8c686f7008c48d32e95fc55ce31b0
lucas gregoir <lgregori@invertironline.com>
2019-06-04 19:02:47 -0300

    mejora automation

--- installAll.sh
+++ installAll.sh
@@ -6,3 +6,5 @@ ansible-playbook teamcityInstall.yml -l servicios
 ansible-playbook elasticSearchKibanaInstall.yml -l servicios
 ansible-playbook dotnetcoreInstall.yml -l webserver
 ansible-playbook filebeat_linux.yml -l webserver
+ansible all -l 10.0.0.60 -m shell -a "docker cp tcserver:/opt/teamcity/logs/teamcity-server.log /root/tc.log"
+ansible all -l 10.0.0.60 -m shell -a "tail /root/tc.log"
